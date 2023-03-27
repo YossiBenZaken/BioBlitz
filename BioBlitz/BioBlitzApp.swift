50603d0bd0ed7f331346449ba6978b8a7fa4476a
YossiBenZaken <yossibz@gmail.com>
2023-03-27 20:26:37 +0300

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Yosef Ben Zaken on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
